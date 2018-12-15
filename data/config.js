9bc766e2b0571d220db81646f4e454a46cf879a3
Maxim Filimonov <tpaktopsp@gmail.com>
2018-12-15 21:18:03 +1000

    update config

--- data/config.js
+++ data/config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
-  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
-  siteRss: '/rss.xml',
-  siteFBAppID: '', // FB Application ID for using app insights
-  googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
-}
+  siteTitle: "Reify Academy", // Site title.
+  siteTitleAlt: "Learn how to build your tech startup", // Alternative site title for SEO.
+  siteLogo: "/icons/icon-512x512.png", // Logo used for SEO and manifest.
+  siteUrl: "https://www2.reify.acadey/", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription:
+    "Start your software business by doing. Learn how to build your tech startup", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml",
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleTagManagerID: "", // GTM tracking ID.
+  disqusShortname: "reify-academy", // Disqus shortname.
+  userName: "Reify Academy",
+  userTwitter: "academyreify",
+  userLocation: "Australia",
+  userDescription: "",
+  copyright: "Copyright © Reify Academy 2018. All Rights Reserved.", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#4cb5ab", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#ffffff", // Used for setting manifest background color.
+};
